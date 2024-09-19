f2cbcfa25f2ae456c1e7cc848639286764d86989
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-09-18 22:51:12 -0400

    Fix organization DTO

--- src/main/java/com/optimagrowth/dto/OrganizationDto.java
+++ src/main/java/com/optimagrowth/dto/OrganizationDto.java
@@ -2,10 +2,7 @@ package com.optimagrowth.organization.model;
 
 import org.springframework.hateoas.RepresentationModel;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 import lombok.Data;
-import lombok.ToString;
 
 @Data
 public class OrganizationDto extends RepresentationModel<OrganizationDto> {
