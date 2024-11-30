6d91dc807899e335de21076ad6e036f84cb5d79a
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-11-30 15:14:56 -0500

    Add element counts to page DTO

--- src/main/java/com/optimagrowth/dto/PageDto.java
+++ src/main/java/com/optimagrowth/dto/PageDto.java
@@ -6,16 +6,20 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(callSuper=true)
+@EqualsAndHashCode(callSuper = true)
 public class PageDto<T> extends CollectionModel<T> {
     private final int number;
     private final int size;
     private final int totalPages;
+    private final int numberOfElements;
+    private final long totalElements;
 
-    public PageDto(Iterable<T> content, int number, int size, int totalPages) {
+    public PageDto(Iterable<T> content, int number, int size, int totalPages, int numberOfElements, long totalElements) {
         super(content);
         this.number = number;
         this.size = size;
         this.totalPages = totalPages;
+        this.numberOfElements = numberOfElements;
+        this.totalElements = totalElements;
     }
 }
